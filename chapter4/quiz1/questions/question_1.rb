53a39c90b1fa9dd825999b3257e8251dcedf0a39
AlistairTait12 <alistair.tait1@gmail.com>
2021-01-13 23:37:13 +0000

    another attempt

--- chapter4/quiz1/questions/question_1.rb
+++ chapter4/quiz1/questions/question_1.rb
@@ -11,14 +11,14 @@
 number = 1
 
 while number <= 20
-    if (number % 3 == 0) && (number % 5 == 0)
-        puts "FizzBuzz"
-    elsif number % 5 == 0
-        puts "Buzz"
-    elsif number % 3 == 0
-        puts "Fizz"
-    else
-        puts number
-    end
-    number += 1
+  if (number % 3 == 0) && (number % 5 == 0)
+      puts 'FizzBuzz'
+  elsif number % 5 == 0
+      puts 'Buzz'
+  elsif number % 3 == 0
+      puts 'Fizz'
+  else
+      puts number.to_s
+  end
+  number += 1
 end
