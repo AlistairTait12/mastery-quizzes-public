6a7bede0f9191ca38c8fd8543ec81690e33d7595
AlistairTait12 <alistair.tait1@gmail.com>
2021-01-19 11:10:55 +0000

    attempted Chapter 10, quiz 1, passed tests

--- chapter10/quiz1/questions/question_1.rb
+++ chapter10/quiz1/questions/question_1.rb
@@ -22,3 +22,41 @@
 
 # * Note: When you run the automated tests, make sure to remove from
 #   the top level of the file any calls to any methods.
+
+class Todo
+  def initialize(string)
+    @string = string
+  end
+
+  def text
+    return @string
+  end
+end
+
+class TodoList
+  def initialize
+    @list = []
+  end
+
+  def add(todo)
+    @list.push(todo.text)
+  end
+
+  def print
+    @all_tasks = String.new
+    @list.each { |item| @all_tasks << "* #{item}\n" }
+    puts @all_tasks
+  end
+end
+
+# my_list = TodoList.new
+
+# task_1 = Todo.new("Buy bread")
+# task_2 = Todo.new("Clean house")
+# task_3 = Todo.new("Call Mam")
+
+# my_list.add(task_1)
+# my_list.add(task_2)
+# my_list.add(task_3)
+
+# my_list.print
